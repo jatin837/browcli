18f6ce026cc2348c912a0ca952c3cd942943b456
dj4t9n <pandeyjatin47@gmail.com>
2021-08-15 13:18:21 +0530

    add logger

--- main.c
+++ main.c
@@ -12,3 +12,63 @@
 #include <sys/time.h>
 #include <sys/ioctl.h>
 #include <netdb.h>
+
+#define SERVER_PORT 9003
+#define MAXLINE 4096
+#define SOCKADDR struct sockaddr
+
+
+void die(const char* log_msg, ...); 
+
+
+
+
+int main(int argc, char** argv){
+	if (argc != 2)
+		die("[USAGE: %s <server ip address>", argv[0]);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void die(const char* log, ...) {
+	int errno_save;
+	va_list ap;
+	errno_save = errno;
+	va_start(ap, log);
+	vfprintf(stdout, log, ap);
+	fprintf(stdout, "\n");
+	fflush(stdout);
+
+	if (errno_save != 0){
+		fprintf(stdout, 
+				"[ERROR NUMBER = %d] : %s\n", 
+				errno_save,
+				strerror(errno_save)
+				);
+		fprintf(stdout, "\n");
+		fflush(stdout);
+	}
+
+	va_end(ap);
+
+	exit(1);
+}
