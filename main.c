834c926ed1ab530db9963eec0b0108400f99007e
dj4t9n <pandeyjatin47@gmail.com>
2021-08-15 13:43:42 +0530

    not working

--- main.c
+++ main.c
@@ -21,42 +21,54 @@
 void die(const char* log_msg, ...); 
 
 
-
-
 int main(int argc, char** argv){
-
+	printf("DONE");
 	if (argc != 2)
-		die("[USAGE: %s <server ip address>", argv[0]);
+		die("[USAGE: %s <server ip address>]", argv[0]);
 
+	printf("DONE");
 	int sockfd, n, sendbytes;
 	struct sockaddr_in servaddr;
 	char sendline[MAXLINE], recvline[MAXLINE];
 
+	printf("DONE");
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		die("ERROR WHILE CREATING SOCKET!");
 
+	printf("DONE");
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERVER_PORT);
 
+	printf("DONE");
+	if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0)
+		die("inet_pton error for %s ", argv[1]);
+
+	printf("DONE");
+	if (connect(sockfd, (SOCKADDR*) &servaddr, sizeof(servaddr)) < 0)
+		die("CONNECTION FAILED");
+
+	printf("DONE");
+	sprintf(sendline, "GET / HTTP/1.1\r\n\r\n");
+	sendbytes = strlen(sendline);
+
+	printf("DONE");
+	if (write(sockfd, sendline, sendbytes) != sendbytes)
+		die("WRITE ERROR");
+
+	printf("DONE");
+	memset(recvline, 0, MAXLINE);
+
+	printf("DONE");
+	
+	printf("DONE");
+	if (n < 0)
+		die("READ ERROR");
+
+	exit(0);
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 void die(const char* log, ...) {
